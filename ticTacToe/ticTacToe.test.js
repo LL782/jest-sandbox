d64d422a1e946ab210e06c56ea989e04df6cc50f
Laurence Lord <laurence.lord@gmail.com>
2020-08-24 18:30:19 +0100

    Improve tests

--- ticTacToe/ticTacToe.test.js
+++ ticTacToe/ticTacToe.test.js
@@ -1,19 +1,21 @@
 const TicTacToe = require("./ticTacToe");
 
 describe("ticTacToe", () => {
-  let ticTacToe;
+  let grid;
   let play;
 
   beforeEach(() => {
     ticTacToe = new TicTacToe();
     play = ticTacToe.play.bind(ticTacToe);
+    grid = ticTacToe.grid;
   });
   it("Player X goes first by placing an X on the grid", () => {
-    ticTacToe.play(0);
-    expect(ticTacToe.grid[0]).toBe("X");
+    play(0);
+    expect(grid[0]).toBe("X");
   });
   it("Player O goes second by placing an O on the grid", () => {
+    play(0);
     play(1);
-    expect(ticTacToe.grid[1]).toBe("O");
+    expect(grid[1]).toBe("O");
   });
 });
